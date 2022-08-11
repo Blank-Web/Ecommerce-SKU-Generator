1b13e75e854d5f5d52496f6749efd7726f49aa33
Ian Datulimbac <iandatulimbac0012@gmail.com>
2022-08-11 17:42:17 +0800

    The Brand SKU section now works but I need to fix some bugs.

--- script/script.js
+++ script/script.js
@@ -1,50 +1,83 @@
 const tr = document.getElementsByTagName('tr');
-var clickTracker = "";
-let trA = [...tr];
-let ian = "das";
+let brandTracker = "";
+let shirtSizeTracker = "";
+let colorTracker = "";
+let printSizeTracker = "";
+let printLocationTracker = "";
 
-/*
-for (let i = 0;i<tr.length;i++){
-    tr[i].addEventListener("click", () => tr[i].style.backgroundColor="#05bb94");
-    clickTracker = tr[i].textContent;
-}
+let brandBody = document.querySelectorAll("#brandBody tr");
 
+var text = "";
+var textC = text.textContent;
 
-function reply_click(clicked_id){
-for (let t of trA){
-    t.addEventListener("click", () => {
-        t.style.backgroundColor="#05bb94";
-        //clickTracker = trA.indexOf(t);
-        clickTracker = clicked_id;
-        alert(`Chosen SKU code: ${clickTracker}`);
-        let p = document.createElement("p");
-        p.innerHTML = `clicked cell: ${clickTracker}`;
-        let twe = document.body.appendChild(p);
+let brandButtonTracker = 0;
 
 
+function saveSkuBrand(clicked_id){
+    brandButtonTracker++;
+    brandTracker = clicked_id;
 
 
+    for (let i=0;i<brandBody.length;i++){
+    brandBody[i].style.backgroundColor="transparent";
+    }
 
+    if (brandTracker == text.textContent){
+        if (brandTracker == ""){
+            document.getElementById(`${brandTracker}`).style.backgroundColor="#05bb94"; //darker green
+        }else{
+        document.getElementById(`${brandTracker}`).style.backgroundColor= "#05bb94"; //cream
         }
-    );
+    } else {
+        document.getElementById(`${brandTracker}`).style.backgroundColor="#05bb94"; //darker green
+    }
 
-}
-}
-*/
+    if (brandTracker !== text.textContent){
+        document.getElementById(textC).style.border="2px solid rgb(245 197 100)";
 
-function reply_click(clicked_id){
-    clickTracker = clicked_id;
-    alert(`Chosen SKU code: ${clickTracker}`);
-    let p = document.createElement("p");
-    p.style.color="red";
-    p.style.fontWeight="bold";
-    p.style.position="relative";
-    p.style.width="100%";
-    p.style.backgroundColor="#e6e6e6";
-    p.innerHTML = `(CLICKED CELL'S SKU CODE: ${clickTracker})`;
-    let twe = document.getElementsByClassName("tablesContainer")[0].prepend(p);
-    document.getElementById(`${clickTracker}`).style.backgroundColor="#05bb94";
+    }
 }
+
+
+async function mouseOver (hovered_id){
+    if (brandButtonTracker == 0){
+        document.getElementById(hovered_id).style.backgroundColor="#83e7d2"; //lighter green
+    } else if (brandButtonTracker !== 0){
+            if (hovered_id !== brandTracker && hovered_id !== text.textContent){
+                document.getElementById(hovered_id).style.backgroundColor="#83e7d2"; //lighter green
+            }else if (hovered_id == brandTracker && hovered_id !== text.textContent){
+                document.getElementById(hovered_id).style.backgroundColor="#05bb94"; //darker green
+            } else if ( hovered_id == text.textContent){
+                document.getElementById(hovered_id).style.backgroundColor="#83e7d2"; //lighter green
+            }
+    }
+}
+
+async function mouseOut (out_id){
+    if (out_id == brandTracker && out_id !== text.textContent){
+        document.getElementById(out_id).style.backgroundColor="#05bb94"; //darker green
+    } else if (out_id == text.textContent){
+        document.getElementById(out_id).style.backgroundColor="rgba(245, 197, 100, 0.2)";
+    } else if ( out_id !== text.textContent && out_id !== brandTracker){
+        document.getElementById(out_id).style.backgroundColor="transparent";
+    } else if (out_id == brandTracker && out_id == text.textContent){
+        document.getElementById(out_id).style.backgroundColor="#05bb94"; //darker green
+    }else {document.getElementById(out_id).style.backgroundColor="transparent";}
+}
+
+
+
+
+async function displaySkuBrand(){
+textC = brandTracker;
+text = document.getElementById("brandSKU");
+document.getElementById(`${brandTracker}`).style.backgroundColor="#05bb94";
+text.innerHTML = `${brandTracker}`;
+document.getElementById(`${brandTracker}`).style.backgroundColor="rgb(245 197 100)";
+brandButtonTracker++;
+}
+
+
     
 
 
